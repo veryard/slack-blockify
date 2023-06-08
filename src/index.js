960362dd8ef020ddce81f3ff3f3500cb738e41ef
Brad <bradveryard@gmail.com>
2023-06-08 16:03:25 +0800

    fix: Remove sillyness

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ const { markdownToBlocks } = require('@tryfabric/mack');
     try {
         const markdown = core.getInput('text', { require: true });
         markdownToBlocks(markdown).then((res) => {
-            let json = JSON.stringify(res, null, 2);
-            // remove first [ and last ]
-            json = json.substring(1, json.length - 1);
-            core.setOutput("text", json);
-            core.info(`JSON: ${json}`);
+            core.setOutput("text", JSON.stringify(res, null, 2));
         })
 
     } catch (error) {
