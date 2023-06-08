ebc656a214f07b748f3c71ff3ed114eba4bca80b
Brad <bradveryard@gmail.com>
2023-06-08 15:33:49 +0800

    chore: Tidy up

--- node_modules/.pnpm/@tryfabric+mack@1.2.1/node_modules/@tryfabric/mack/build/src/slack.d.ts
+++
@@ -1,5 +0,0 @@
-import type { DividerBlock, HeaderBlock, ImageBlock, SectionBlock } from '@slack/types';
-export declare function section(text: string): SectionBlock;
-export declare function divider(): DividerBlock;
-export declare function header(text: string): HeaderBlock;
-export declare function image(url: string, altText: string, title?: string): ImageBlock;
