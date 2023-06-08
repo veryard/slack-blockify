f708672cb7f17aeddfa071ef3328ac29afe88331
Brad <bradveryard@gmail.com>
2023-06-08 15:45:51 +0800

    fix(output): Remove [] for slackapi

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const { markdownToBlocks } = require('@tryfabric/mack');
     try {
         const markdown = core.getInput('text', { require: true });
         markdownToBlocks(markdown).then((res) => {
-            core.setOutput("text", JSON.stringify(res, null, 2));
+            let json = JSON.stringify(res, null, 2);
+            // remove first [ and last ]
+            json = json.substring(1, json.length - 1);
+            core.setOutput("json", json);
         })
 
     } catch (error) {
